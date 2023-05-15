c18c7a34b3c4cabde565f58330bc8b947ee5b7b8
Saidafzalkhon <Saidafzalkhon.developer@gmail.com>
2023-05-15 17:32:56 +0500

    Add file

---
+++ src/main/java/uz/java/musicschool/MusicSchoolApplication.java
@@ -0,0 +1,13 @@
+package uz.java.musicschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/java/musicschool/MusicSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.java.musicschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
